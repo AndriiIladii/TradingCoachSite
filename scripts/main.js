d1f1ced4120c814f78f113749e7aea152785bf17
Andrii_Iladii <iladiy21@gmail.com>
2025-07-28 15:31:42 +0300

    2 sections done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
 import "../styles/main.scss";
+
+function startProgress() {
+  const progress = document.querySelector(".outer");
+  const data = document.querySelector(".number");
+
+  let interval = null;
+  let index = 0;
+
+  interval = setInterval(() => {
+    if (index >= 90) {
+      clearInterval(interval);
+    } else {
+      index++;
+      data.innerText = `${index}%`;
+      progress.style.background = `conic-gradient(#029837 ${
+        index * 3.6
+      }deg, #f7fff6 ${index * 3}deg)`;
+    }
+  }, 30);
+}
+
+startProgress();
