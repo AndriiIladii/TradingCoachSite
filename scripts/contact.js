28446c992abcfc0e88e27065e3d898fb6bddd8fc
= <iladiy21@gmail.com>
2025-08-12 01:37:46 +0300

    adaptive done

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,1 +1,16 @@
 import "../styles/main.scss";
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".burger__menu");
+const menuLinks = document.querySelectorAll(".burger__link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,19 @@ accordionBox.forEach((item) => {
     item.classList.toggle("active");
   });
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".burger__menu");
+const menuLinks = document.querySelectorAll(".burger__link");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  });
+});
